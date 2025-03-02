aa5732955134432c875db4c9016df2438d085c3e
Muhammad Yanuarullah Assidiq <myanuarullah20@gmail.com>
2025-03-02 21:25:33 +0700

    fix font

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,6 +4,7 @@ import { Plus_Jakarta_Sans } from "next/font/google";
 
 const jakartaSans = Plus_Jakarta_Sans({
   weight: ["300", "400", "500", "600", "700", "800"],
+  subsets: ["latin"],
 });
 
 export const metadata = {
